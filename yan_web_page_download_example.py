edc1bbe04af0d160efb3ab093607b959404e0314
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-03-17 22:48:51 +0400

    Update yan_web_page_download_example.py

--- yan_web_page_download_example.py
+++ yan_web_page_download_example.py
@@ -1,7 +1,7 @@
 import yan_web_page_download 
 
 jessica_web_page_download.download_page_from_url(
-	page_url = 'https://www.linkedin.com/company/digital14/about/',
+	page_url = 'https://www.linkedin.com/company/microsoft/about/',
 	curl_file = '/Downloads/curl_linkedin_company.sh')
 
 jessica_web_page_download.download_page_from_url(
