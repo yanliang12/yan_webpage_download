7756a3e24eb157cefeb1c860381a5a323d77b68f
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-03-17 22:41:18 +0400

    Add files via upload

---
+++ yan_web_page_download_example.py
@@ -0,0 +1,8 @@
+import yan_web_page_download 
+
+jessica_web_page_download.download_page_from_url(
+	page_url = 'https://www.linkedin.com/company/digital14/about/',
+	curl_file = '/Downloads/curl_linkedin_company.sh')
+
+jessica_web_page_download.download_page_from_url(
+	page_url = 'https://www.myqbd.com/qatar-companies/maha-art')
