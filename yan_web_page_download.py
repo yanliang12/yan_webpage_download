28580d0e7197f01353a87463fb342ba3b0f2d985
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-03-17 22:40:09 +0400

    Add files via upload

---
+++ yan_web_page_download.py
@@ -0,0 +1,48 @@
+#######yan_web_page_download.py#######
+import re
+import os
+import html
+import random
+import pandas
+import hashlib 
+import urllib
+
+def download_page_from_url(
+	page_url,
+	curl_file = None):
+	try:
+		temp_html = "temp_%f.html"%(random.random())
+		if curl_file is None:
+			curl_commend_new = u"""
+				curl '%s' --compressed -o %s
+				"""%(page_url, temp_html)
+		else:
+			curl_commend = open(curl_file).read()
+			'''
+			replace the page url of the curl file by the input page url
+			'''
+			re_curl_firstline = r'^curl \'(?P<page_url>[^\s]+)\' \\\n'
+			curl_fristline = re.search(re_curl_firstline,
+				curl_commend).group()
+			curl_page_url = re.search(re_curl_firstline,
+				curl_commend).group('page_url')
+			curl_fristline_replaced = re.sub(
+				re.escape(curl_page_url),
+				page_url,
+				curl_fristline)
+			curl_commend_new = re.sub(
+				re.escape(curl_fristline),
+				curl_fristline_replaced,
+				curl_commend)
+			curl_commend_new += " -o %s"%(temp_html)
+		os.system(curl_commend_new)
+		html_date = open(temp_html).read()
+		html_date = html.unescape(html_date)
+		try:
+			os.remove(temp_html)
+		except:
+			pass
+		return html_date
+	except:
+		return None
+#######yan_web_page_download.py#######
