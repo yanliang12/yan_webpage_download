a14113b958cf2c7e0ec0905522590494bd1e9840
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-03-17 23:11:46 +0400

    Update yan_web_page_download_example.py

--- yan_web_page_download_example.py
+++ yan_web_page_download_example.py
@@ -2,7 +2,7 @@ import yan_web_page_download
 
 yan_web_page_download.download_page_from_url(
 	page_url = 'https://www.linkedin.com/company/microsoft/about/',
-	curl_file = '/Downloads/curl_linkedin_company.sh')
+	curl_file = 'curl_page.sh')
 
 yan_web_page_download.download_page_from_url(
 	page_url = 'https://www.myqbd.com/qatar-companies/maha-art')
