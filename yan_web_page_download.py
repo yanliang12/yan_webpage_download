2beaa4f16e1344113331d618a07b3276530061cc
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-04-23 15:59:45 +0400

    Update yan_web_page_download.py

--- yan_web_page_download.py
+++ yan_web_page_download.py
@@ -7,6 +7,8 @@ import urllib
 import hashlib 
 import requests
 
+from PIL import Image
+
 try:
 	import html
 except:
@@ -63,4 +65,61 @@ def download_page_from_url(
 		return html_date
 	except:
 		return None
+
+
+def download_image_from_url(
+	page_url,
+	curl_file = None,
+	redirect = None):
+	if redirect is not None:
+		page_url = requests.get(page_url).url
+	temp_html = "temp_%f"%(random.random())
+	try:
+		if curl_file is None:
+			curl_commend_new = u"""
+				curl '%s' --compressed -o %s
+				"""%(page_url, temp_html)
+		else:
+			curl_commend = open(curl_file).read()
+			'''
+			replace the page url of the curl file by the input page url
+			'''
+			re_curl_firstline = r'^curl \'(?P<page_url>[^\s]+)\' \\\n'
+			curl_fristline = re.search(re_curl_firstline,
+				curl_commend).group()
+			curl_page_url = re.search(re_curl_firstline,
+				curl_commend).group('page_url')
+			curl_fristline_replaced = re.sub(
+				re.escape(curl_page_url),
+				page_url,
+				curl_fristline)
+			curl_commend_new = re.sub(
+				re.escape(curl_fristline),
+				curl_fristline_replaced,
+				curl_commend)
+			curl_commend_new += " -o %s"%(temp_html)
+		os.system(curl_commend_new)
+		image_end = Image.open(temp_html).format
+		url_hash = str_md5(page_url)
+		file_name = '%s.%s'%(url_hash,image_end)
+		os.rename(temp_html, file_name)
+		return file_name
+	except:
+		return None
+
+'''
+file_name = download_image_from_url(
+	page_url = 'https://media-exp1.licdn.com/dms/image/C4D0BAQFjQ2av29r_1Q/company-logo_400_400/0/1589448600484?e=1626912000&v=beta&t=jJlYbU6S7ErXsdhOC9V0y-WnBXzKRKGIrIi463Gz7SM',
+	curl_file = None,
+	redirect = None)
+
+os.system("mv %s /Downloads/"%(file_name))
+
+file_name = download_image_from_url(
+	page_url = 'https://www.gravatar.com/avatar/47c20752720b3597f9370f9dcf5e21b4?s=32&d=identicon&r=PG&f=1',
+	curl_file = None,
+	redirect = None)
+
+os.system("mv %s /Downloads/"%(file_name))
+'''
 #######yan_web_page_download.py#######
