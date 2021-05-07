ede37f66e5a32829c2d4444b50d7b9838a6ad28a
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-05-07 09:04:20 +0400

    Delete yan_web_page_download_example.py

--- yan_web_page_download_example.py
+++
@@ -1,8 +0,0 @@
-import yan_web_page_download 
-
-yan_web_page_download.download_page_from_url(
-	page_url = 'https://www.linkedin.com/company/microsoft/about/',
-	curl_file = 'curl_page.sh')
-
-yan_web_page_download.download_page_from_url(
-	page_url = 'https://www.myqbd.com/qatar-companies/maha-art')
